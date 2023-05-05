3cddb4e11b4821c72662c7bac84025cb80231fb3
Riccardo Piredda <127589992+Nemonek@users.noreply.github.com>
2023-05-05 13:03:24 +0200

    Initial commit

---
+++ piramide.cs
@@ -0,0 +1,27 @@
+using System;
+namespace Piramide{
+    public static class Piramide {
+        public static int Piani( int mattoni )
+        {
+            int tmp = 0;
+            int i = 0;
+
+            //Nel ciclo for viene fatto il controllo per il valore di i successivo: se il valore di i^2 + tmp è
+            //minore di mattoni, aggiunge a tutti gli effetti quel calcolo a tmp
+            for (i = 1; tmp + (i*i) <= mattoni ; i+=2)
+                tmp += (i*i);
+            
+            return i/2;
+        }
+        public static int Rimanenti( int mattoni )
+        {
+            int tmp = 0;
+            for (int i = 1; tmp + (i*i) <= mattoni ; i+=2)
+                tmp += (i*i);
+
+            return mattoni - tmp;
+
+        }
+
+    }
+}
